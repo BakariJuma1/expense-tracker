5e982820b7cbc80bc6effd01f59fead18a27d102
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-04-12 12:14:45 +0300

    adding a delete button

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -2,28 +2,30 @@ import React from "react";
 
 function ExpenseTable({ expenses }) {
   return (
-    <table className="expenseTable">
-      <thead>
-        <tr>
-          <th>Expense</th>
-          <th>Description</th>
-          <th>Category</th>
-          <th>Amount</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {expenses.map((exp) => (
-          <tr key={exp.id}>
-            <td>{exp.name}</td>
-            <td>{exp.description}</td>
-            <td>{exp.category}</td>
-            <td>{exp.amount}</td>
-            <td>{exp.date}</td>
+    <div className="tableContainer">
+      <table className="expenseTable">
+        <thead>
+          <tr>
+            <th>Expense</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Date</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {expenses.map((exp) => (
+            <tr key={exp.id}>
+              <td>{exp.name}</td>
+              <td>{exp.description}</td>
+              <td>{exp.category}</td>
+              <td>{exp.amount}</td>
+              <td>{exp.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
